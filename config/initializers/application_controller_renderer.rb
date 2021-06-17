90ff99610d1b9bb48619abd8a2d0bbc5669cec0a
EC2 Default User <ec2-user@ip-172-31-3-253.ap-northeast-1.compute.internal>
2021-06-17 13:26:04 +0000

    テスト

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,6 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'f708ff61bc78a94d6f712c4f260e1f3238ad091bf30ffbcaa5f44b1a4e93ad9cb55e223094b437bcc8e7c1af57f0371d39641cd96c6354ebbf08dd6ce1aaf561'
+
